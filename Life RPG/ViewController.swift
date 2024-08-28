c97fa3f2a78d89ebb1e1d02b5a885825a91e03e9
PaingThuTa <paingthuta472@gmail.com>
2024-08-28 16:28:00 +0700

    Initial Commit

---
+++ Life RPG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life RPG
+//
+//  Created by Paing Thu Ta on 27/8/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
