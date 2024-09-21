9524edc1351aebfc1ebb6b2a0d070c5d4c76183d
Sorracha <109853255+beausorracha@users.noreply.github.com>
2024-09-22 01:04:28 +0700

    Added /WelcomeVC, /DefaultVC and /HistoryQuestVC
    WelcomeVC = To select language
    DefaultVC = To see default rank
    HistoryQuestVC = Quest Info in History

---
+++ Life RPG/DefaultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultViewController.swift
+//  Life RPG
+//
+//  Created by Beau on 22/9/2567 BE.
+//
+
+import UIKit
+
+class DefaultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Life RPG/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  Life RPG
+//
+//  Created by Beau on 22/9/2567 BE.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
